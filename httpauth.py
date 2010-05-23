dc8534e0db46843679253cac13c8b9691d61b04c
Roy van de Water <roy.v.water@gmail.com>
2010-05-23 11:47:06 -0700

    Redesigned httpauth module in OO fashion

--- httpauth.py
+++ httpauth.py
@@ -3,25 +3,42 @@ import base64
 import string
 import httplib
 
-def get_https_response(server,page,username,password):
+class HttpAuth:
     """
-    "Basic" authentication encodes userid:password in base64. Note
-    that base64.encodestring adds some extra newlines/carriage-returns
-    to the end of the result. string.strip is a simple way to remove
-    these characters.
+    Class to interact with an https service that may or may not
+    require authentication
     """
-    auth = 'Basic ' + string.strip(base64.encodestring(username + ':' + password))
+    def __init__(self, server, ssl=True, username=None, password=None):
+        """
+        If username and password are not provided, https request
+        will not send authentication headers
+        """
+        self.server   = server
 
-    # h = httplib.HTTP(server)
-    connection = httplib.HTTPSConnection(server)
-    connection.connect()
-    connection.putrequest('GET', ensure_path(page))
-    connection.putheader('Authorization', auth )
-    connection.endheaders()
-    return connection.getresponse()
+        authstring = 'Basic ' + string.strip(base64.encodestring(username + ':' + password))
+        self.auth  = authstring if username and password else None
 
-def ensure_path(page):
-    """
-    Ensures that the path starts with a '/'
-    """
-    return page if page.startswith('/') else "/{0}".format(page)
+        self.connection_method = httplib.HTTPSConnection if ssl else httplib.HTTPConnection
+
+    def get_https_response(self, page):
+        """
+        "Basic" authentication encodes userid:password in base64. Note
+        that base64.encodestring adds some extra newlines/carriage-returns
+        to the end of the result. string.strip is a simple way to remove
+        these characters.
+        """
+
+        # h = httplib.HTTP(server)
+        connection = self.connection_method(self.server)
+        connection.connect()
+        connection.putrequest('GET', self.ensure_path(page))
+        if self.auth:
+            connection.putheader('Authorization', self.auth )
+        connection.endheaders()
+        return connection.getresponse()
+
+    def ensure_path(self, page):
+        """
+        Ensures that the path starts with a '/'
+        """
+        return page if page.startswith('/') else "/{0}".format(page)
--- jsonsole.py
+++ jsonsole.py
@@ -11,13 +11,17 @@ def main(argv):
     config_file = file("config.json")
     config = json.loads(config_file.read())
 
-    server   = config["connection"]["server"]
-    username = config["connection"]["username"]
-    password = config["connection"]["password"]
+    connection = config["connection"]
+    server   = connection["server"]
+    username = connection["username"]
+    password = connection["password"]
+    ssl = True if connection["ssl"].lower() == "true" else False
+
+    http = httpauth.HttpAuth(server, ssl, username, password)
 
     while True:
         page = raw_input(">>>")
-        response = httpauth.get_https_response(server,page,username,password)
+        response = http.get_https_response(page)
 
         json_response = response.read()
         remote_data = json.loads(json_response)
@@ -28,5 +32,5 @@ if __name__=="__main__":
     try:
         main(sys.argv[1:])
     except KeyboardInterrupt:
-        print()
+        print("")
         sys.exit(0)
